91fe62d5f350644c55eab48e584825a13b87476c
Jim Hayes <jhayes@sdsc.edu>
2017-09-02 22:24:53 -0700

    Add clang and clang tools to installation.
    Reesolves APS-535.

--- src/llvm/version.mk
+++ src/llvm/version.mk
@@ -8,7 +8,7 @@ endif
 
 NAME           = sdsc-llvm
 VERSION        = 4.0.1
-RELEASE        = 0
+RELEASE        = 1
 PKGROOT        = /opt/llvm
 
 SRC_SUBDIR     = llvm
@@ -19,6 +19,24 @@ SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).src.$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_NAME)-$(SOURCE_VERSION).src
 
-TAR_XZ_PKGS    = $(SOURCE_PKG)
+CLANG_NAME     = cfe
+CLANG_SUFFIX   = tar.xz
+CLANG_VERSION  = $(VERSION)
+CLANG_PKG      = $(CLANG_NAME)-$(CLANG_VERSION).src.$(CLANG_SUFFIX)
+CLANG_DIR      = $(CLANG_NAME)-$(CLANG_VERSION).src
+
+LLDB_NAME      = lldb
+LLDB_SUFFIX    = tar.xz
+LLDB_VERSION   = $(VERSION)
+LLDB_PKG       = $(LLDB_NAME)-$(LLDB_VERSION).src.$(LLDB_SUFFIX)
+LLDB_DIR       = $(LLDB_NAME)-$(LLDB_VERSION).src
+
+TOOLS_NAME     = clang-tools-extra
+TOOLS_SUFFIX   = tar.xz
+TOOLS_VERSION  = $(VERSION)
+TOOLS_PKG      = $(TOOLS_NAME)-$(TOOLS_VERSION).src.$(TOOLS_SUFFIX)
+TOOLS_DIR      = $(TOOLS_NAME)-$(TOOLS_VERSION).src
+
+TAR_XZ_PKGS    = $(SOURCE_PKG) $(CLANG_PKG) $(LLDB_PKG) $(TOOLS_PKG)
 
 RPM.EXTRAS     = AutoReq:No
